bc181cef8c48ca432c706c4562573aa509b7d286
Seiei Higa <hanachin@gmail.com>
2014-05-14 02:11:22 +0900

    Idobataに投稿出来るようになったよ、yey!

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,5 +1,9 @@
 'use strict';
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log(request);
+    var source = request.title + " " + request.url;
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", decodeURIComponent(request.hook_url), true);
+    xhr.setRequestHeader("Content-Type" , "application/x-www-form-urlencoded");
+    xhr.send('source=' + encodeURIComponent(source));
 });
